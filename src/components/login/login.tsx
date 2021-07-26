06a54bfe660b8078ac91b2bfeea122123e4cf23e
harshit-kakkar <kharshit0@gmail.com>
2021-07-26 21:55:50 +0530

    Created Login page

--- src/components/login/login.tsx
+++ src/components/login/login.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import './loginStyles.css'
 
 function Login() {
   return (
-    <div>
-      <h1>Login page</h1>
+    <div className="login-container">
+      <h1 className="login-header">Log in to your account</h1>
+      <form className="login-form-container">
+          <input className="login-input" placeholder="Email address"></input>
+          <input type="password" className="login-input" placeholder="Password"></input>
+          <button type="button" className="login-button">Log in</button>
+      </form>
       
     </div>
   );
