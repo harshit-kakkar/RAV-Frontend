b0d272162235794874fb1f4a91fb3f97ab9e3461
harshit-kakkar <kharshit0@gmail.com>
2021-07-26 10:39:49 +0530

    Setup react router

--- src/components/mainSection/mainSection.tsx
+++ src/components/mainSection/mainSection.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import './mainSectionStyles.css';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import Signup from '../signup/signup';
+import Login from '../login/login';
 
 function MainSection() {
   return (
-    <div className="main-section-container">
-      Main
-      
-    </div>
+    <Router>
+      <div className="main-section-container">
+        <Switch>
+          <Route path="/" exact component={Signup} /> {/* TODO: Should be changed to homepage when that component is created */}
+          <Route path="/signup" component={Signup} />
+          <Route path="/login" component={Login} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
