afe758682d849507cfb42c11564f72a931822b8d
harshit-kakkar <kharshit0@gmail.com>
2021-07-27 20:36:58 +0530

    Basic layout for signup page completed

--- src/components/signup/signup.tsx
+++ src/components/signup/signup.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import "./signupStyles.css"
 
 function Signup() {
   return (
-    <div>
-      <h1>Signup page</h1>
+    <div className="signup-container">
+      <h1 className="signup-header">Welcome to RAV</h1>
+      <form className="signup-form-container">
+          <input className="signup-input" placeholder="Full Name"></input>
+          <input className="signup-input" placeholder="Email Address"></input>
+          <input type="password" className="signup-input" placeholder="Password"></input>
+
+          <div className="is-mentor-container">
+            <input type="checkbox" className="is-mentor-checkbox"></input>
+            <label>Become a mentor ?</label>
+          </div>
+
+          <button type="button" className="signup-button">Sign up</button>
+      </form>
       
     </div>
   );
