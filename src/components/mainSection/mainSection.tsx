b2d54da9fc73dbc7c8a7d73843c9032be75df541
harshit-kakkar <kharshit0@gmail.com>
2021-07-28 23:47:11 +0530

    Created default redirect to signup

--- src/components/mainSection/mainSection.tsx
+++ src/components/mainSection/mainSection.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './mainSectionStyles.css';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import {BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom'
 import Signup from '../signup/signup';
 import Login from '../login/login';
 
@@ -9,9 +9,11 @@ function MainSection() {
     <Router>
       <div className="main-section-container">
         <Switch>
-          <Route path="/" exact component={Signup} /> {/* TODO: Should be changed to homepage when that component is created */}
+          {/* <Route path="/" exact component={Signup} /> 
+          TODO: Should be changed to homepage when that component is created */}
           <Route path="/signup" component={Signup} />
           <Route path="/login" component={Login} />
+          <Route render={() => <Redirect to={{pathname: "/signup"}} />} />
         </Switch>
       </div>
     </Router>
