e1b0e0690f6d0b8ee7323b76f53d1e9f87e1a2af
harshit-kakkar <kharshit0@gmail.com>
2021-08-15 01:31:22 +0530

    Implemented calling signup API

--- src/components/signup/signup.tsx
+++ src/components/signup/signup.tsx
@@ -4,14 +4,54 @@ import {RootState} from '../../reducers'
 import {useSelector} from 'react-redux'
 // import {isMentorSignup} from '../../actions/IsMentorSignupActions'
 import MentorSignup from '../mentorSignup/mentorSignup';
+import { useHistory } from 'react-router-dom';
 
 function Signup() {
-  const signupScheduleState = useSelector((state: RootState) => state.signupSchedule)
+  const history = useHistory();
+
+  const signupScheduleState: any = useSelector((state: RootState) => state.signupSchedule)
   const [isMentorSignupState, setIsMentorSignupState] = useState(false);
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [domain, setDomain] = useState('');
+
+  async function sendSignupRequest(){
+   let signupScheduleRequestFormat:any = [] 
+    let signupScheduleRequestKeys: Array<string> = Object.keys(signupScheduleState);
+    signupScheduleRequestKeys.map((day) => {
+      if(signupScheduleState[day]["checked"]){
+        let daySchedule = {
+          "startTimeHour": signupScheduleState[day]["startTime"],
+          "endTimeHour": signupScheduleState[day]["endTime"],
+          "day": day
+        }
+        signupScheduleRequestFormat.push(daySchedule);
+      }
+    })
+
+    let signupRequestData = {
+      "name": name,
+      "email": email, 
+      "password": password,
+      "domain": domain,
+      "isMentor": isMentorSignupState,
+      "schedule": signupScheduleRequestFormat
+    }
+
+    console.log(signupRequestData)
+    let signupResponse = await fetch('http://localhost:8080/signup', { 
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(signupRequestData)
+    })
+
+    if(signupResponse.status === 200){
+      history.push("/login")
+    }
+  } 
 
   return (
     <div className="signup-container">
@@ -48,7 +88,7 @@ function Signup() {
           </div>
           {isMentorSignupState?<MentorSignup setDomain={setDomain} />: <div></div>}
 
-          <button type="button" className="signup-button" onClick={() => console.log(signupScheduleState)}>Sign up</button>
+          <button type="button" className="signup-button" onClick={() => sendSignupRequest()}>Sign up</button>
       </form>
       
     </div>
